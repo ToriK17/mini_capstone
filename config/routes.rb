3808668672be432e5d7eccfb92f4043219407e8b
ToriK17 <vfcad@protonmail.com>
2019-09-27 10:47:37 -0700

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,34 +16,18 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do 
-
-   get "/products" => "products#index"
-
+    get "/products" => "products#index"
     get "/products/:id" => "products#show"
-
     post "/products" => "products#create"
-
     patch "/products/:id" => "products#update"
-    
     delete "/products/:id" => "products#destroy"
-
     post "/users" => "users#create"
-
-    
     post "/sessions" => "sessions#create"
-
-    
     get "/orders" => "orders#index"
-    
     get "/orders/:id" => "orders#show"
-    
     post "/orders" => "orders#create"
-
-
     post "/carted_products" => "carted_products#create"
-    
     get "/carted_products" => "carted_products#index"
-    
     delete "/carted_products/:id" => "carted_products#destroy"
   end 
 end 
